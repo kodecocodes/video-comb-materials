08566f20a97e8b035e511d54188154945a99a76b
Angel Buenrostro <abuenrostro@boomtownroi.com>
2022-07-20 16:47:30 -0400

    combine publishers + subscribers Notion tutorial notes page: https://sudsy-stocking-559.notion.site/Subscriber-Operators-Subjects-5c4a250ca2c14ce0ad25292ade3e0ea3

--- 02-publishers-and-subscribers/starter/Begin.playground/Contents.swift
+++ 02-publishers-and-subscribers/starter/Begin.playground/Contents.swift
@@ -3,8 +3,65 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+example(of: "NotificationCenter") {
 
+    let center = NotificationCenter.default
+    let myNotification = Notification.Name("MyNotification")
+
+    let publisher = center
+        .publisher(for: myNotification, object: nil)
+
+    let subscription = publisher
+        .print()
+        .sink { _ in
+            print("Notification received from a publisher")
+        }
+
+    center.post(name: myNotification, object: nil)
+    subscription.cancel()
+}
+
+example(of: "Just") {
+
+    let just = Just("Hello World")
+
+    just
+        .sink(
+        receiveCompletion: {
+            print("Received Completion", $0)
+        },
+        receiveValue: {
+            print("Received Value", $0)
+        })
+        .store(in: &subscriptions)
+}
+
+
+example(of: "assign(to:on:)") {
+    class SomeObject {
+        var value: String = "" {
+            didSet {
+                print(value)
+            }
+        }
+    }
+
+    let object = SomeObject()
+
+    ["Hello", "World!"].publisher
+        .assign(to: \.value, on: object)
+        .store(in: &subscriptions)
+}
+
+example(of: "PassthroughSubject") {
+    let subject = PassthroughSubject<String, Never>()
+    /// this type of publisher needs explicitly declared types because it is initialized without starting values
+    /// which the compiler cannot infer as it did with the standard `Publisher` type
+
+    subject
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
 /// Copyright (c) 2020 Razeware LLC
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
